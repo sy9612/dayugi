3ebbea05411df21a39349e26e4cd33160f44d450
박영창 <kin16742@naver.com>
2021-04-26 15:31:37 +0900

    FE : project 폴더 구조 수정

--- FE/Dayugi/App.js
+++ FE/Dayugi/App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 
 export default function App() {
