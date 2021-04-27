e7eae1d78ef8f9ba04057e969c21b9ff76d585ac
sy9612 <sy9612@naver.com>
2021-04-27 23:16:52 +0900

    BE : 다이어리 삭, 조회 서비스 구현

--- BE/Dayugi/src/main/java/com/ssafy/dayugi/repository/DiaryRepository.java
+++ BE/Dayugi/src/main/java/com/ssafy/dayugi/repository/DiaryRepository.java
@@ -1,11 +1,20 @@
 package com.ssafy.dayugi.repository;
 
 import com.ssafy.dayugi.model.entity.Diary;
+import com.ssafy.dayugi.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface DiaryRepository extends JpaRepository<Diary, Long> {
-    public int deleteDiaryBy;
-    public int deleteDiariesBy;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
-
+public interface DiaryRepository extends JpaRepository<Diary, User, Long> {
+    public Optional<Diary> findDiaryByDid(int did);//did에 해당하는 다이어리 찾기
+    public List<Optional<Diary>> findDiariesByUid(int uid);//사용자가 작성한 다이어리 전체 조회
+    @Query(value = "select * from diary where uid= :uid and YEAR(diary_date) = :year and MONTH(diary_date) = :month", nativeQuery = true)
+    public List<Optional<Diary>> findByUserAndDate(@Param("uid") int uid, @Param("year")  int year, @Param("month")  int month);//uid, year, month 일치하는 다이어리 전체 조회
+    public int deleteDiaryByDid(int did);//다이어리 한 개 삭제
+    public int deleteDiariesByUid(int uid);//사용자가 작성한 다이어리 전체 삭제
 }
--- BE/Dayugi/src/main/java/com/ssafy/dayugi/service/DiaryServiceImpl.java
+++ BE/Dayugi/src/main/java/com/ssafy/dayugi/service/DiaryServiceImpl.java
@@ -2,9 +2,15 @@ package com.ssafy.dayugi.service;
 
 import com.ssafy.dayugi.model.entity.Diary;
 import com.ssafy.dayugi.repository.DiaryRepository;
+import org.hibernate.annotations.NamedQueries;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.Entity;
+import javax.persistence.NamedQuery;
+import javax.persistence.Query;
+import javax.persistence.Table;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,30 +19,64 @@ public class DiaryServiceImpl implements DiaryService{
 
     @Autowired
     private DiaryRepository diaryRepository;
+//    private PlantRepository plantRepository;
 
     @Override
     public boolean writeDiary(Diary diary) throws Exception {
+        //한줄평 전처리는 어떻게 되는거지??
+        //사진 올릴 경우도 생각해야함
+        diaryRepository.save(diary);//다이어리 저장
+//        Optional<Plant> plant = plantRepository.findPlantByUid(uid);
+//        int experience = plant.get().getExpereience(); //식물 경험치
+//        plant.get().setExpereince();
+        //디비에 저장할 때 경험치 상승
+        //경험치 다 차면 레벨 상승, 다육이 이벤트 추가
         return false;
     }
 
     @Override
     public boolean updateDiary(Diary diary) throws Exception {
+//        Optional<Diary> prevDiary = diaryRepository.findDiaryByDid();
+//        if(prevDiary.isPresent()){
+//            prevDiary.get().setDiary_date(diary.getDiary_date());
+//            prevDiary.get().setDiary_content(diary.getDiary_content());
+//        }
         return false;
     }
 
+    //완료
     @Override
-    public List<Diary> readDiary(int did) throws Exception {
-        return null;
+    public Optional<Diary> readDiary(int did) throws Exception {
+        Optional<Diary> diary = diaryRepository.findDiaryByDid(did);
+        return diary;
     }
 
     @Override
-    public List<Diary> monthDiary(int year, int month) throws Exception {
-        return null;
+    public List<Optional<Diary>> monthDiary(int uid, int year, int month) throws Exception {
+        List<Optional<Diary>> diaries = diaryRepository.findByUserAndDate(uid, year, month);
+        return diaries;
     }
 
+    //완료
     @Override
     public boolean deleteDiary(int did) throws Exception {
-        Optional<Diary> diary = diaryRepository
+        Optional<Diary> diary = diaryRepository.findDiaryByDid(did);
+
+        if(diary.isPresent()){
+            diaryRepository.deleteDiaryByDid(did);
+            return true;
+        }
+        return false;
+    }
+
+    //완료
+    @Override
+    public boolean deleteAllDiary(int uid) throws Exception {
+        List<Optional<Diary>> diaries = diaryRepository.findDiariesByUid(uid);
+        if(!diaries.isEmpty()){
+            diaryRepository.deleteDiariesByUid(uid);
+            return true;
+        }
         return false;
     }
 }
