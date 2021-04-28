ce2b1b70e611e6a9c52d087bb3bafaabce00cd1c
박영창 <kin16742@naver.com>
2021-04-28 13:28:48 +0900

    FE : 모아보기 페이지 레이아웃 구

--- FE/Dayugi/src/screens/DiaryArchivePage.js
+++ FE/Dayugi/src/screens/DiaryArchivePage.js
@@ -1,23 +1,78 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 import CustomHeader from '../components/CustomHeader';
+import Separator from '../components/Separator';
 
 class DiaryArchivePage extends React.Component {
+  state = {
+    contents:[
+      {
+          diaryDate:"2000-01-01",
+          diaryContent:"안녕안녕",
+      },
+      {
+          diaryDate:"2011-01-01",
+          diaryContent:"안녕안녕안녕안녕",
+      },
+      {
+          diaryDate:"2111-01-01",
+          diaryContent:"안녕안녕안녕안녕안녕안녕",
+      },
+      {
+          diaryDate:"2222-01-01",
+          diaryContent:"안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕",
+      },
+    ]
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <CustomHeader navigation = {this.props.navigation}/>
-        <Text>모아보기 페이지</Text>
+        <FlatList
+          style = {{ width : "100%" }}
+          data = {this.state.contents}
+          renderItem = {({ item }) => 
+            <Item
+              item = {item}
+              navigation = {this.props.navigation}
+            />
+          }
+          keyExtractor = {item => item.date}
+        />
       </View>
     );
   }
 }
 
+function Item({ item, navigation }) {
+  return (
+    <TouchableOpacity style = {styles.drawerContentListItem} onPress={() => alert(item.diaryDate + " " + item.diaryContent)}>
+        <Text style = {styles.drawerContentItemDiaryDate}>{item.diaryDate}</Text>
+        <Separator />
+        <Text style = {styles.drawerContentItemDiaryContent}>{item.diaryContent}</Text>
+    </TouchableOpacity>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     width: '100%',
+  },
+  drawerContentListItem:{
+    backgroundColor : '#fff',
+    padding: 8,
+    margin: 8,
+    marginBottom: 0,
+    borderRadius: 5,
+  },
+  drawerContentItemDiaryDate:{
+    fontSize : 12,
+  },
+  drawerContentItemDiaryContent:{
+    fontSize : 14,
   },
 });
 
