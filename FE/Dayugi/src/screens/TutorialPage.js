c23dbdecbaf327b93cf59a1381f5104eca8a6777
박영창 <kin16742@naver.com>
2021-04-30 16:42:57 +0900

    FE : 튜토리얼 페이지 디자인 변경

--- FE/Dayugi/src/screens/TutorialPage.js
+++ FE/Dayugi/src/screens/TutorialPage.js
@@ -1,46 +1,94 @@
 import React from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
+import { StyleSheet, View, Text, Button, StatusBar } from 'react-native'
 import ViewPager from "@react-native-community/viewpager"
 
 class TutorialPage extends React.Component {
   render () {
     return (
-      <ViewPager
-        style={{ flex: 1 }}
-        initialPage={0}>
-        <View style={styles.pageStyle}>
-            <Text>1번</Text>
-        </View>
-        <View style={styles.pageStyle}>
-            <Text>2번</Text>
-        </View>
-        <View style={styles.pageStyle}>
-            <Text>3번</Text>
-        </View>
-        <View style={styles.pageStyle}>
-            <Text>4번</Text>
-            <Button
-                style={styles.tutorialEndButton}
-                title="시작하기"
-                onPress={() => {this.props.navigation.navigate("DiaryCalendar")}}
-            />
-        </View>
-      </ViewPager>
+        <ViewPager
+            style={{ flex: 1 }}
+            initialPage={0}>
+            <StatusBar barStyle="dark-content" />
+            <View style={styles.pageStyle}>
+                <View style={styles.tutorialImage}>
+                    <Text>이미지 1</Text>
+                </View>
+                <View style={styles.tutorialText}>
+                    <Text style={{ fontSize: 16 }}>안녕하세요 옆으로 드래그해서 움직여보세요</Text>
+                </View>
+                <View style={styles.tutorialBottom}>
+                </View>
+            </View>
+            <View style={styles.pageStyle}>
+                <View style={styles.tutorialImage}>
+                    <Text>이미지 2</Text>
+                </View>
+                <View style={styles.tutorialText}>
+                    <Text style={{ fontSize: 16 }}>일기를 써봐요 ~~ 기능 설명 짧게</Text>
+                </View>
+                <View style={styles.tutorialBottom}>       
+                </View>
+            </View>
+            <View style={styles.pageStyle}>   
+                <View style={styles.tutorialImage}>
+                    <Text>이미지 3</Text>
+                </View>
+                <View style={styles.tutorialText}>
+                    <Text style={{ fontSize: 16 }}>메뉴는 화면 왼쪽을 드래그하면 열 수 있어요</Text>
+                </View>
+                <View style={styles.tutorialBottom}>  
+                </View>
+            </View>
+            <View style={styles.pageStyle}> 
+                <View style={styles.tutorialImage}>
+                    <Text>이미지 4</Text>
+                </View>
+                <View style={styles.tutorialText}>
+                    <Text style={{ fontSize: 16 }}>이제 일기 쓰러 가요</Text>
+                </View>
+                <View style={styles.tutorialButton}>
+                    <Button
+                        color='#fff'
+                        title="시작하기"
+                        onPress={() => {this.props.navigation.navigate("DiaryCalendar")}}
+                    />
+                </View>
+            </View>
+        </ViewPager>
     )
   }
 }
 
 const styles = StyleSheet.create({
   pageStyle: {
-    backgroundColor: '#D7B397',
     justifyContent: 'center',
     alignItems: 'center',
+    width: '100%',
     padding: 20,
     flex: 1,
   },
-  tutorialEndButton: {
-    position: 'absolute',
-    bottom: 8,
+  tutorialImage: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    flex: 8,
+  },
+  tutorialText: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    flex: 3,
+  },
+  tutorialBottom: {
+    flex: 1,
+  },
+  tutorialButton: {
+    backgroundColor: '#007AFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    borderRadius: 5,
+    flex: 1,
   }
 })
 
