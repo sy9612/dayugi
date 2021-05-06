9b77be4aeeb9a38b1d82864081191f0750f6fc97
jinpyo hong <hojp7874@naver.com>
2021-05-06 16:39:22 +0900

    ML-summarize : Create summarize model

--- ML/app.py
+++ ML/app.py
@@ -1,12 +1,13 @@
 # app.py
 from flask import Flask, render_template
 
+
 #Flask 객체 인스턴스 생성
 app = Flask(__name__)
 
 @app.route('/') # 접속하는 url
 def index():
-  return render_template('index.html')
+  return 'Hello, world'
 
 if __name__=="__main__":
   app.run(debug=True)
