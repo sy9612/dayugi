f5586b9b021299e85d6eee80811df75f9f097e30
Ubuntu <ubuntu@ip-172-26-15-241.ap-northeast-2.compute.internal>
2021-05-10 10:42:48 +0000

    develop : download pth file

--- ML/module/checkpoint/download.py
+++ ML/module/checkpoint/download.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
     root_path = str(pathlib.Path(__file__).parent.absolute())
 
     kogpt2_file_id = '1jcVcLnnV2jL-j0gGKishdwfZywqg8R80'
-    koelectra_file_id = '1tMhB3mL-Y1KlRL4LZzdgNpIKeiLU-eG_'
+    konert_file_id = '1uJzQzktpNaI9-PtWe2i4VQguLmMml8zh'
 
     kogpt2_destination = root_path + '/kogpt2-wellness-auto-regressive.pth'
-    koelectra_destination = root_path + '/koelectra-wellness-text-classification.pth'
+    kobert_destination = root_path + '/kobert_emotion_classification.pth'
 
     kogpt2_url = 'https://drive.google.com/file/d/1jcVcLnnV2jL-j0gGKishdwfZywqg8R80/uc?export=download'
-    koelectra_url = 'https://drive.google.com/file/d/1tMhB3mL-Y1KlRL4LZzdgNpIKeiLU-eG_/uc?export=download'
+    kobert_url = 'https://drive.google.com/file/d/1uJzQzktpNaI9-PtWe2i4VQguLmMml8zh/uc?export=download'
     
     gdd.download_file_from_google_drive(file_id=kogpt2_file_id, dest_path=kogpt2_destination, showsize=True)
-    gdd.download_file_from_google_drive(file_id=koelectra_file_id, dest_path=koelectra_destination, showsize=True)
+    gdd.download_file_from_google_drive(file_id=kobert_file_id, dest_path=kobert_destination, showsize=True)
