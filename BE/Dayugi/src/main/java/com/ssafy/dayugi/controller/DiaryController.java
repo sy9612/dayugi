bf25268b9075f32069152826619d658d8966b65e
이소영 <sy9612@naver.com>
2021-05-14 15:31:53 +0000

    Update DiaryController.java

--- BE/Dayugi/src/main/java/com/ssafy/dayugi/controller/DiaryController.java
+++ BE/Dayugi/src/main/java/com/ssafy/dayugi/controller/DiaryController.java
@@ -125,7 +125,7 @@ public class DiaryController {
                     String filename = new MD5Generator(origFilename).toString();
                     // 실행되는 위치의 'files' 폴더에 파일이 저장됩니다.
 //                    String savePath = System.getProperty("user.dir") + "\\files";
-                    String savePath = "/home/ubuntu/share/nginx/html";
+                    String savePath = "./files";
 
                     // 파일이 저장되는 폴더가 없으면 폴더를 생성합니다.
                     if (!new File(savePath).exists()) {
