41a905ce51eb62b18427e8cf1dbe4924720598da
sy9612 <sy9612@naver.com>
2021-05-17 02:23:18 +0900

    BE : 다이어리 수정 api 변경_ver4

--- BE/Dayugi/src/main/java/com/ssafy/dayugi/repository/DiaryFileRepository.java
+++ BE/Dayugi/src/main/java/com/ssafy/dayugi/repository/DiaryFileRepository.java
@@ -9,10 +9,13 @@ import java.util.Optional;
 
 public interface DiaryFileRepository extends JpaRepository<DiaryFile, Long> {
     public Optional<DiaryFile> findDiaryFileByFid(int fid);//파일 id로 파일 조회
+    public DiaryFile findDiaryFileByDiary_Did(int did);//일기 did에 해당하는 다이어리 파일 찾기
     public List<DiaryFile> findDiaryFilesByDiary_Did(int did);//일기 did에 해당하는 다이어리 파일 찾기
     public List<DiaryFile> findDiaryFilesByUser_Uid(int uid);//일기 did에 해당하는 다이어리 파일 찾기
     @Transactional
     public int deleteDiaryFilesByDiary_Did(int did);//다이어리 id로 파일 전부 삭제
+    @Transactional
+    public int deleteDiaryFileByDiary_Did(int did);//다이어리 id로 파일 전부 삭제
     @Transactional
     public int deleteDiaryFileByFid(int fid);//파일 id로 해당 파일만 삭제
     @Transactional
