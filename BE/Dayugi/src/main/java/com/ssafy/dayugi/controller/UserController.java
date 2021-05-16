5a79384980d111b95220f767107d0f97b88f3478
HJ W <woohjm@naver.com>
2021-05-17 00:39:03 +0900

    BE-jwt : 토큰 재발급 api 추가

--- BE/Dayugi/src/main/java/com/ssafy/dayugi/controller/UserController.java
+++ BE/Dayugi/src/main/java/com/ssafy/dayugi/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -143,6 +144,35 @@ public class UserController extends GlobalControllerAdvice{
             entity = new ResponseEntity<>(result, HttpStatus.ACCEPTED);
         }
 
+        return entity;
+    }
+
+    @PostMapping(value = "/token")
+    private ResponseEntity tokenReissue(@RequestBody Map map, @ApiIgnore final Authentication authentication) throws Exception{
+        ResponseEntity entity = null;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        Map result = new HashMap();
+//        int tokenUid = jwtTokenProvider.getUserPk(authentication.getPrincipal());
+        int uid = (Integer)map.get("uid");
+        if(((User) authentication.getPrincipal()).getUid()==uid){
+            String email = ((User) authentication.getPrincipal()).getEmail();
+
+            result.put("success", "success");
+
+            String token = jwtTokenProvider.createToken(uid);
+            if (StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
+                Authentication newAuthentication = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(newAuthentication);
+                logger.info("{} 로그인 정보를 저장했습니다", email);
+                result.put("Authorization",("Bearer " + token));
+                entity = new ResponseEntity<>(result,httpHeaders, HttpStatus.OK);
+            }
+            httpHeaders.add("Authorization", "Bearer " + token);
+        }else{
+            result.put("success", "fail");
+            result.put("message", "토큰과 일치하지 않는 회원 정보");
+            entity = new ResponseEntity<>(result, HttpStatus.FORBIDDEN);
+        }
         return entity;
     }
 
